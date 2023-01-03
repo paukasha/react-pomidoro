32bb700b7eb445a73e384d45296bf2c2b0c75881
Дарья <odinpozitivchik@mail.ru>
2023-01-03 15:30:03 +1000

    Доработано:  настройки  - валидация полей

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -14,12 +14,17 @@ import useLocalStorageState from "use-local-storage-state";
 const Settings = () => {
     const [settings, setSettings] = useLocalStorageState('settings', {
         defaultValue: [
-            {sysName: 'tomatos', text: 'Макс. кол-во помидоров в одной задаче', value: 4},
-            {sysName: 'tomatoDuration', text: 'Продолжительность помидора, минуты', value: 5},
-            {sysName: 'smallBreakDuration', text: 'Продолжительность короткого перерыва, минуты', value: 2},
-            {sysName: 'bigBreakDuration', text: 'Продолжительность длинного перерыва, минуты', value: 3},
-            {sysName: 'notifs', text: 'Включить уведомления', value: true},
-            {sysName: 'audio', text: 'Включить звук', value: false},
+            {sysName: 'tomatos', text: 'Макс. кол-во помидоров в одной задаче', value: 4, error: false},
+            {sysName: 'tomatoDuration', text: 'Продолжительность помидора, минуты', value: 5, error: false},
+            {
+                sysName: 'smallBreakDuration',
+                text: 'Продолжительность короткого перерыва, минуты',
+                value: 2,
+                error: false
+            },
+            {sysName: 'bigBreakDuration', text: 'Продолжительность длинного перерыва, минуты', value: 3, error: false},
+            {sysName: 'notifs', text: 'Включить уведомления', value: true, error: false},
+            {sysName: 'audio', text: 'Включить звук', value: false, error: false},
         ]
     })
 
@@ -30,12 +35,27 @@ const Settings = () => {
     const handleChange = (value: number | boolean, sysName: string | boolean) => {
         window.clearTimeout(timer as any)
 
+        if (!value && typeof value !== 'boolean') {
+            let newSettings = settings.map(el => {
+                if (el.sysName === sysName) {
+                    return {
+                        ...el,
+                        error: true
+                    }
+                }
+                return el
+            })
+            setSettings([...newSettings])
+            return
+        }
+
         const newTimer = window.setTimeout(() => {
             let newSettings = settings.map(el => {
                 if (el.sysName === sysName) {
                     return {
                         ...el,
-                        value: value
+                        value: value,
+                        error: false
                     }
                 }
                 return el
@@ -63,9 +83,11 @@ const Settings = () => {
                         return (
                             <TextField key={el.sysName}
                                        fullWidth
+                                       error={el.error}
                                        required
                                        id="outlined-required"
                                        label={el.text}
+                                       helperText={el.error && 'Введите положительное число'}
                                        defaultValue={el.value}
                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(+e.target.value, el.sysName)}
                             />
@@ -81,7 +103,6 @@ const Settings = () => {
                         )
                     }
                 })
-
             }
 
             <Snackbar open={isSnackBarOpen}
